4c7b95ede830dcb73fa937b4f3e6e8ea6943b32a
letrungtin09 <letrungtin1312@gmail.com>
2024-03-19 10:17:12 +0700

    upload sources code TryOnClothesOnline project

---
+++ models/ChoseItem.js
@@ -0,0 +1,8 @@
+function ChoseItem(id, type, name, desc, imgsrc_jpg, imgsrc_png){
+    this.id = id;
+    this.type = type;
+    this.name = name;
+    this.desc = desc;
+    this.imgSrc_jpg = imgsrc_jpg;
+    this.imgSrc_png = imgsrc_png;
+}
